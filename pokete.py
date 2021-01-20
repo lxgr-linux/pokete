614190f4d9132f21fde49dcee11800f0cffc10d1
lxgr-linux <lxgr@protonmail.com>
2021-01-20 18:17:46 +0100

    added labels to objects

--- pokete.py
+++ pokete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# This software is licensed under the GPL3
 
 import scrap_engine as se
 import random, time
@@ -35,6 +36,7 @@ pokes={
     }
 }
 
+
 class Poke():
     def __init__(self, poke, lvl, player=True):
         self.lvl = lvl
@@ -52,6 +54,10 @@ class Poke():
             self.atc += attacs[attac]["atcbetter"]
             self.ap -= attacs[attac]["atcap"]
 
+
+enemy = Poke("steini", 5, player=False)
+player = Poke("steini", 5)
+
 fightmap = se.Map(background=" ")
 line_left = se.Square("|", 1, fightmap.height-7)
 line_right = se.Square("|", 1, fightmap.height-7)
@@ -60,14 +66,14 @@ line_middle = se.Square("_", fightmap.width-2, 1)
 
 e_underline = se.Text("----------------+")
 e_sideline = se.Square("|", 1, 3)
-e_ap = se.Text("AP: ")
-e_hp = se.Text("HP: ")
-e_lvl = se.Text("Lvl: ")
-e_name = se.Text("")
-p_ap = se.Text("AP: ")
-p_hp = se.Text("HP: ")
-p_lvl = se.Text("Lvl: ")
-p_name = se.Text("")
+enemy.text_ap = se.Text("AP: ")
+enemy.text_hp = se.Text("HP: ")
+enemy.text_lvl = se.Text("Lvl: ")
+enemy.text_name = se.Text("")
+player.text_ap = se.Text("AP: ")
+player.text_hp = se.Text("HP: ")
+player.text_lvl = se.Text("Lvl: ")
+player.text_name = se.Text("")
 
 p_upperline = se.Text("+----------------")
 p_sideline = se.Square("|", 1, 4)
@@ -77,33 +83,31 @@ line_top.add(fightmap, 0, 0)
 
 e_underline.add(fightmap, 1, 4)
 e_sideline.add(fightmap, len(e_underline.text), 1)
-e_name.add(fightmap, 1, 1)
-e_lvl.add(fightmap, 1, 2)
-e_ap.add(fightmap, 1, 3)
-e_hp.add(fightmap, 10, 3)
+enemy.text_name.add(fightmap, 1, 1)
+enemy.text_lvl.add(fightmap, 1, 2)
+enemy.text_ap.add(fightmap, 1, 3)
+enemy.text_hp.add(fightmap, 10, 3)
 
 p_upperline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-11)
 p_sideline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-10)
-p_name.add(fightmap, fightmap.width-17, fightmap.height-10)
-p_lvl.add(fightmap, fightmap.width-17, fightmap.height-9)
-p_ap.add(fightmap, fightmap.width-17, fightmap.height-8)
-p_hp.add(fightmap, fightmap.width-8, fightmap.height-8)
+player.text_name.add(fightmap, fightmap.width-17, fightmap.height-10)
+player.text_lvl.add(fightmap, fightmap.width-17, fightmap.height-9)
+player.text_ap.add(fightmap, fightmap.width-17, fightmap.height-8)
+player.text_hp.add(fightmap, fightmap.width-8, fightmap.height-8)
 
 line_middle.add(fightmap, 1, fightmap.height-7)
 fightmap.show(init=True)
 
-enemy = Poke("steini", 5, player=False)
-e_name.rechar(str(enemy.name))
-e_lvl.rechar("Lvl:"+str(enemy.lvl))
-e_ap.rechar("AP:"+str(enemy.ap))
-e_hp.rechar("HP:"+str(enemy.hp))
+enemy.text_name.rechar(str(enemy.name))
+enemy.text_lvl.rechar("Lvl:"+str(enemy.lvl))
+enemy.text_ap.rechar("AP:"+str(enemy.ap))
+enemy.text_hp.rechar("HP:"+str(enemy.hp))
 enemy.ico.add(fightmap, fightmap.width-14, 2)
 
-player = Poke("steini", 5)
-p_name.rechar(str(player.name))
-p_lvl.rechar("Lvl:"+str(player.lvl))
-p_ap.rechar("AP:"+str(player.ap))
-p_hp.rechar("HP:"+str(player.hp))
+player.text_name.rechar(str(player.name))
+player.text_lvl.rechar("Lvl:"+str(player.lvl))
+player.text_ap.rechar("AP:"+str(player.ap))
+player.text_hp.rechar("HP:"+str(player.hp))
 player.ico.add(fightmap, 3, fightmap.height-11)
 
 fightmap.show()
@@ -112,22 +116,13 @@ players = [player, enemy]
 while player.hp > 0 and enemy.hp > 0:
     for ob in players:
         if ob.hp <= 0:
-            if ob.player:
-                p_hp.rechar("HP:0")
-            else:
-                e_hp.rechar("HP:0")
+            ob.text_hp.rechar("HP:0")
             break
         ob.attack("tackle", [i for i in players if i != ob][0])
-        if ob.player:
-            p_name.rechar(str(player.name))
-            p_lvl.rechar("Lvl:"+str(player.lvl))
-            p_ap.rechar("AP:"+str(player.ap))
-            p_hp.rechar("HP:"+str(player.hp))
-        else:
-            e_name.rechar(str(enemy.name))
-            e_lvl.rechar("Lvl:"+str(enemy.lvl))
-            e_ap.rechar("AP:"+str(enemy.ap))
-            e_hp.rechar("HP:"+str(enemy.hp))
+        ob.text_name.rechar(str(ob.name))
+        ob.text_lvl.rechar("Lvl:"+str(ob.lvl))
+        ob.text_ap.rechar("AP:"+str(ob.ap))
+        ob.text_hp.rechar("HP:"+str(ob.hp))
         fightmap.show()
         time.sleep(0.5)
     fightmap.show()
