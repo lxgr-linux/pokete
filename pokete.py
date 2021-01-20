d6a49e03c63868bc9dd825247017ffbde7f41d8a
lxgr-linux <lxgr@protonmail.com>
2021-01-20 15:14:13 +0100

    added player

--- pokete.py
+++ pokete.py
@@ -38,6 +38,10 @@ e_ap = se.Text("AP: ")
 e_hp = se.Text("HP: ")
 e_lvl = se.Text("Lvl: ")
 e_name = se.Text("")
+p_ap = se.Text("AP: ")
+p_hp = se.Text("HP: ")
+p_lvl = se.Text("Lvl: ")
+p_name = se.Text("")
 
 p_upperline = se.Text("+----------------")
 p_sideline = se.Square("|", 1, 4)
@@ -54,6 +58,11 @@ e_hp.add(fightmap, 10, 3)
 
 p_upperline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-11)
 p_sideline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-10)
+p_name.add(fightmap, fightmap.width-17, fightmap.height-10)
+p_lvl.add(fightmap, fightmap.width-17, fightmap.height-9)
+p_ap.add(fightmap, fightmap.width-17, fightmap.height-8)
+p_hp.add(fightmap, fightmap.width-8, fightmap.height-8)
+
 line_middle.add(fightmap, 1, fightmap.height-7)
 fightmap.show(init=True)
 
@@ -63,6 +72,13 @@ e_lvl.rechar("Lvl:"+str(enemy.lvl))
 e_ap.rechar("AP:"+str(enemy.ap))
 e_hp.rechar("HP:"+str(enemy.hp))
 enemy.ico.add(fightmap, fightmap.width-14, 2)
+
+player = Poke("steini", 10)
+p_name.rechar(str(player.name))
+p_lvl.rechar("Lvl:"+str(player.lvl))
+p_ap.rechar("AP:"+str(player.ap))
+p_hp.rechar("HP:"+str(player.hp))
+player.ico.add(fightmap, 3, fightmap.height-11)
 
 fightmap.show()
 
