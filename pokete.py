8908cca74919b945130e6cec09f938a6db82175c
lxgr-linux <lxgr@protonmail.com>
2021-01-21 23:47:27 +0100

    corrected numbers

--- pokete.py
+++ pokete.py
@@ -25,8 +25,10 @@ class Poke():
             oldhp = enem.hp
             oldap = self.ap
             enem.hp = round(enem.hp - (self.atc * attac.factor / enem.defense)*random.choice([0.75, 1, 1.26]))
-            self.defense += attac.defbetter
-            self.atc += attac.atcbetter
+            self.defense += attac.pdefbetter
+            self.atc += attac.patcbetter
+            enem.defense += attac.edefbetter
+            enem.atc += attac.eatcbetter
             self.ap -= attac.ap
             outp.rechar(self.name+"("+("you" if self.player else "enemy")+") used "+attac.name+" against "+enem.name+"("+("you" if not self.player else "enemy")+")")
             while oldhp > enem.hp and oldhp > 0:
@@ -82,161 +84,39 @@ else:
             with Listener(on_press=on_press) as listener:
                 listener.join()
 
-attacs={
-    "tackle": {
-        "name": "Tackle",
-        "factor": 3/2,
-        "defbetter": 0,
-        "atcbetter": 0,
-        "ap": 5,
-    },
-    "politure": {
-        "name": "Politure",
-        "factor": 0,
-        "defbetter": 5,
-        "atcbetter": 5,
-        "ap": 5,
-    },
-}
-
-for attac in attacs:
-    exec(attac+"=Attack(attac)")
-
-pokes={
-    "steini": {
-        "ap": "self.lvl+20",
-        "name": "Steini",
-        "hp": "self.lvl*2",
-        "atc": "self.lvl+2",
-        "defense": "4+self.lvl/3",
-        "attacs": [tackle, politure],
-        "ico": """ +-------+
- | o   o |
- |  www  |
- +-------+ """,
-    },
-    "vogli": {
-        "ap": "self.lvl+20",
-        "name": "Vogli",
-        "hp": "self.lvl*2",
-        "atc": "self.lvl+6",
-        "defense": "self.lvl/3",
-        "attacs": [tackle],
-        "ico":"""    A
-   <')
-    www*
-    ||     """
-    },
-    "karpi": {
-        "ap": "self.lvl+5",
-        "name": "Karpi",
-        "hp": "self.lvl*3/2",
-        "atc": "self.lvl",
-        "defense": "self.lvl/4",
-        "attacs": [tackle],
-        "ico":"""
-
-  <°))))><
-           """
-    },
-    "würgos": {
-        "ap": "self.lvl+5",
-        "name": "Würgos",
-        "hp": "self.lvl*3/2",
-        "atc": "self.lvl",
-        "defense": "self.lvl/4",
-        "attacs": [tackle],
-        "ico": """ {{{{{{{{{
-  }}}}}}}
-  >'({{{
-           """
-    },
-}
-
-
-ev=""
-os.system("")
-recognising=threading.Thread(target=recogniser)
-recognising.daemon=True
-recognising.start()
-
 def fight():
     global ev, attack, fightmap, outp
 
     enemy = Poke(random.choice([i for i in pokes]), 5, player=False)
     player = Poke(random.choice([i for i in pokes]), 6)
 
-    fightmap = se.Map(background=" ")
-    line_left = se.Square("|", 1, fightmap.height-7)
-    line_right = se.Square("|", 1, fightmap.height-7)
-    line_top = se.Square("_", fightmap.width, 1)
-    line_top_text_box = se.Square("-", fightmap.width-2, 1)
-    line_bottom_text_box = se.Square("-", fightmap.width-2, 1)
-    line_middle = se.Square("_", fightmap.width-2, 1)
-    line_l_text_box = se.Text("+\n|\n|\n+")
-    line_r_text_box = se.Text("+\n|\n|\n+")
-    e_underline = se.Text("----------------+")
-    e_sideline = se.Square("|", 1, 3)
-    p_upperline = se.Text("+----------------")
-    p_sideline = se.Square("|", 1, 4)
-    outp = se.Text("")
-
-    enemy.text_ap = se.Text("AP: ")
-    enemy.text_hp = se.Text("HP: ")
-    enemy.text_lvl = se.Text("Lvl: ")
-    enemy.text_name = se.Text("")
-    player.text_ap = se.Text("AP: ")
-    player.text_hp = se.Text("HP: ")
-    player.text_lvl = se.Text("Lvl: ")
-    player.text_name = se.Text("")
-
-    line_left.add(fightmap, 0, 1)
-    line_right.add(fightmap, fightmap.width-1, 1)
-    line_top.add(fightmap, 0, 0)
-    line_top_text_box.add(fightmap, 1, fightmap.height-6)
-    line_bottom_text_box.add(fightmap, 1, fightmap.height-3)
-    line_l_text_box.add(fightmap, 0, fightmap.height-6)
-    line_r_text_box.add(fightmap, fightmap.width-1, fightmap.height-6)
-    outp.add(fightmap, 1, fightmap.height-5)
-    e_underline.add(fightmap, 1, 4)
-    e_sideline.add(fightmap, len(e_underline.text), 1)
-    p_upperline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-11)
-    p_sideline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-10)
-    line_middle.add(fightmap, 1, fightmap.height-7)
+    enemy.text_name = se.Text(str(enemy.name))
+    enemy.text_ap = se.Text("AP:"+str(enemy.ap))
+    enemy.text_hp = se.Text("HP:"+str(enemy.hp))
+    enemy.text_lvl = se.Text("Lvl:"+str(enemy.lvl))
+    player.text_ap = se.Text("AP:"+str(player.ap))
+    player.text_hp = se.Text("HP:"+str(player.hp))
+    player.text_lvl = se.Text("Lvl:"+str(player.lvl))
+    player.text_name = se.Text(str(player.name))
 
     enemy.text_name.add(fightmap, 1, 1)
     enemy.text_lvl.add(fightmap, 1, 2)
     enemy.text_ap.add(fightmap, 1, 3)
     enemy.text_hp.add(fightmap, 10, 3)
-
+    enemy.ico.add(fightmap, fightmap.width-14, 2)
     player.text_name.add(fightmap, fightmap.width-17, fightmap.height-10)
     player.text_lvl.add(fightmap, fightmap.width-17, fightmap.height-9)
     player.text_ap.add(fightmap, fightmap.width-17, fightmap.height-8)
     player.text_hp.add(fightmap, fightmap.width-8, fightmap.height-8)
-
-    fightmap.show(init=True)
-
-    enemy.text_name.rechar(str(enemy.name))
-    enemy.text_lvl.rechar("Lvl:"+str(enemy.lvl))
-    enemy.text_ap.rechar("AP:"+str(enemy.ap))
-    enemy.text_hp.rechar("HP:"+str(enemy.hp))
-    enemy.ico.add(fightmap, fightmap.width-14, 2)
-
-    player.text_name.rechar(str(player.name))
-    player.text_lvl.rechar("Lvl:"+str(player.lvl))
-    player.text_ap.rechar("AP:"+str(player.ap))
-    player.text_hp.rechar("HP:"+str(player.hp))
     player.ico.add(fightmap, 3, fightmap.height-11)
 
     player.atc_labels = []
-
     for i, atc in enumerate(player.attacs):
         player.atc_labels.append(se.Text(str(i)+": "+atc.name))
-
     for ob, x, y in zip(player.atc_labels, [1, 1, 7, 7], [fightmap.height-2, fightmap.height-1, fightmap.height-2, fightmap.height-1]):
         ob.add(fightmap, x, y)
 
-    fightmap.show()
+    fightmap.show(init=True)
 
     players = [player, enemy]
     while player.hp > 0 and enemy.hp > 0:
@@ -272,7 +152,142 @@ def fight():
     time.sleep(1)
 
 def main():
+    ev=""
+    os.system("")
+    recognising=threading.Thread(target=recogniser)
+    recognising.daemon=True
+    recognising.start()
+
     fight()
+
+
+attacs={
+    "tackle": {
+        "name": "Tackle",
+        "factor": 3/2,
+        "pdefbetter": 0,
+        "patcbetter": 0,
+        "edefbetter": 0,
+        "eatcbetter": 0,
+        "ap": 2,
+    },
+    "bite": {
+        "name": "Bite",
+        "factor": 1.75,
+        "pdefbetter": 0,
+        "patcbetter": 0,
+        "edefbetter": 0,
+        "eatcbetter": 0,
+        "ap": 2,
+    },
+    "politure": {
+        "name": "Politure",
+        "factor": 0,
+        "pdefbetter": 5,
+        "patcbetter": 5,
+        "edefbetter": 0,
+        "eatcbetter": 0,
+        "ap": 3,
+    },
+    "chocer": {
+        "name": "Chocer",
+        "factor": 1,
+        "pdefbetter": 0,
+        "patcbetter": 0,
+        "edefbetter": 0,
+        "eatcbetter": -3,
+        "ap": 5,
+    },
+}
+
+for attac in attacs:
+    exec(attac+"=Attack(attac)")
+
+pokes={
+    "steini": {
+        "name": "Steini",
+        "ap": "25",
+        "hp": "25",
+        "atc": "self.lvl+2",
+        "defense": "self.lvl+4",
+        "attacs": [tackle, politure],
+        "ico": """ +-------+
+ | o   o |
+ |  www  |
+ +-------+ """,
+    },
+    "vogli": {
+        "name": "Vogli",
+        "ap": "25",
+        "hp": "20",
+        "atc": "self.lvl+6",
+        "defense": "self.lvl+1",
+        "attacs": [tackle],
+        "ico":"""    A
+   <')
+    www*
+    ||     """
+    },
+    "karpi": {
+        "name": "Karpi",
+        "ap": "25",
+        "hp": "15",
+        "atc": "self.lvl",
+        "defense": "self.lvl/2",
+        "attacs": [tackle],
+        "ico":"""
+
+  <°))))><
+           """
+    },
+    "würgos": {
+        "name": "Würgos",
+        "ap": "25",
+        "hp": "20",
+        "atc": "self.lvl+1",
+        "defense": "self.lvl",
+        "attacs": [chocer, bite],
+        "ico": """ {{{{{{{{{
+  }}}}}}}
+  >'({{{
+           """
+    },
+}
+
+# objects relevant for fight()
+fightmap = se.Map(background=" ")
+line_left = se.Square("|", 1, fightmap.height-7)
+line_right = se.Square("|", 1, fightmap.height-7)
+line_top = se.Square("_", fightmap.width, 1)
+line_top_text_box = se.Square("-", fightmap.width-2, 1)
+line_bottom_text_box = se.Square("-", fightmap.width-2, 1)
+line_middle = se.Square("_", fightmap.width-2, 1)
+line_l_text_box = se.Text("+\n|\n|\n+")
+line_r_text_box = se.Text("+\n|\n|\n+")
+e_underline = se.Text("----------------+")
+e_sideline = se.Square("|", 1, 3)
+p_upperline = se.Text("+----------------")
+p_sideline = se.Square("|", 1, 4)
+outp = se.Text("")
+line_left.add(fightmap, 0, 1)
+line_right.add(fightmap, fightmap.width-1, 1)
+line_top.add(fightmap, 0, 0)
+line_top_text_box.add(fightmap, 1, fightmap.height-6)
+line_bottom_text_box.add(fightmap, 1, fightmap.height-3)
+line_l_text_box.add(fightmap, 0, fightmap.height-6)
+line_r_text_box.add(fightmap, fightmap.width-1, fightmap.height-6)
+outp.add(fightmap, 1, fightmap.height-5)
+e_underline.add(fightmap, 1, 4)
+e_sideline.add(fightmap, len(e_underline.text), 1)
+p_upperline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-11)
+p_sideline.add(fightmap, fightmap.width-1-len(p_upperline.text), fightmap.height-10)
+line_middle.add(fightmap, 1, fightmap.height-7)
+
+ev=""
+os.system("")
+recognising=threading.Thread(target=recogniser)
+recognising.daemon=True
+recognising.start()
 
 if __name__ == "__main__":
     try:
