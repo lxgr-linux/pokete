66af74584157b733cc6d701ee50fd67a26df1c99
lxgr-linux <lxgr@protonmail.com>
2021-06-05 10:18:48 +0200

    fixed bug with trainers seeing through stuff

--- pokete.py
+++ pokete.py
@@ -102,7 +102,7 @@ class Trainer(se.Object):
     def do(self, map):
         if figure.x == self.x and self.poke.hp > 0 and self.will:
             arr = []
-            for i in range(figure.y+2 if figure.y < self.y else self.y+1, self.y if figure.y < self.y else figure.y-2):
+            for i in range(figure.y+1 if figure.y < self.y else self.y+1, self.y if figure.y < self.y else figure.y):
                 arr += map.obmap[i][self.x]
             if any(ob.state == "solid" for ob in arr):
                 return
