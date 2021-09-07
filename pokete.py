d7eeb283dcf7217be42a58e161ff4922adf5da96
lxgr-linux <lxgr@protonmail.com>
2021-09-07 17:46:45 +0200

    Fixed teleport with no return

--- pokete.py
+++ pokete.py
@@ -1618,13 +1618,12 @@ def playmap_23_npc_8():
 ################
 
 def teleport():
-    if type(obj := roadmap(choose=True)) is None:
+    if (obj := roadmap(choose=True)) is None:
         return
     else:
         cen_d = map_data[obj.name]["hard_obs"]["pokecenter"]
-        Dor("#", state="float",
-                    arg_proto={"map": eval(obj.name),
-                                "x": cen_d["x"]+5, "y": cen_d["y"]+6}).action(None)
+        Dor("", state="float", arg_proto={"map": eval(obj.name),
+                "x": cen_d["x"]+5, "y": cen_d["y"]+6}).action(None)
 
 
 def swap_poke():
