dd66c9299f929b2fc22031bc5639b4c459ff5bb2
lxgr-linux <lxgr@protonmail.com>
2021-09-28 18:11:46 +0200

    Added dicko

--- pokete_data/poketes.py
+++ pokete_data/poketes.py
@@ -814,13 +814,36 @@ WW\/o o\/WW
         "lose_xp": 2,
         "rarity": 1,
         "types": ["ground", "normal"],
-        "evolve_poke": "",
-        "evolve_lvl": 0,
+        "evolve_poke": "dicko",
+        "evolve_lvl": 35,
         "initiative": "self.lvl()+2",
         "ico": [{
             "txt": """    
     __
    ('')
+   |  |""",
+            "esc": None}
+        ]
+    },
+    "dicko": {
+        "name": "Dicko",
+        "hp": 25,
+        "atc": "self.lvl()+3",
+        "defense": "self.lvl()+5",
+        "attacks": ["tackle", "dick_energy", "hiding", "ground_hit"],
+        "pool": [],
+        "miss_chance": 0.1,
+        "desc": "An even bigger what ever, that sticks out of the ground.",
+        "lose_xp": 2,
+        "rarity": 1,
+        "types": ["ground", "normal"],
+        "evolve_poke": "",
+        "evolve_lvl": 0,
+        "initiative": "self.lvl()+3",
+        "ico": [{
+            "txt": """    __
+   ('')
+   |  |
    |  |""",
             "esc": None}
         ]
