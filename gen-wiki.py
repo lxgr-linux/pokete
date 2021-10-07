0bf71212234c7e6be83c9ad7cda4440c0b02cca5
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2021-10-07 23:06:49 +0200

    Moved the gen_wiki function into the class Wiki This is to make the generation process clearer and prepare for the reuse of code for the multi-page wiki.
    The wiki can still be generated with the Wiki.single() call.
    
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- gen-wiki.py
+++ gen-wiki.py
@@ -3,62 +3,77 @@
 import os
 import release
 import scrap_engine as se
-from pokete_classes.effects import effects, effect_list 
+from pokete_classes.effects import effects, effect_list
 from pokete_data import *
 
 
-def gen_wiki():
-    md_str = f"""v{release.VERSION}
+class Wiki:
+    @staticmethod
+    def start() -> str:
+        return f"""v{release.VERSION}
 
 # Pokete wiki
 This wiki/documentation is a compilation of all Poketes/attacks/types present in the Pokete game.
 This wiki can be generated using ```$ ./gen-wiki.py```.
 
-## Table of contents
+"""
+
+    @staticmethod
+    def table_of_contents() -> str:
+        out = """## Table of contents
 1. [Poketes](#poketes)
 """
 
-    # Table of contents
-    for i, typ in enumerate(sorted(types)):
-        md_str += f"""   {i + 1}. [{typ.capitalize()} Poketes](#{typ}-poketes)\n"""
-        for j, poke in enumerate([k for k in sorted(list(pokes)[1:]) if pokes[k]["types"][0] == typ]):
-            md_str += f"""       {j + 1}. [{pokes[poke]["name"]}](#{poke.replace("_", "-")})\n"""
-    md_str += "2. [Attacks](#attacks)\n"
-    for i, typ in enumerate(sorted(types)):
-        md_str += f"""   {i + 1}. [{typ.capitalize()} attacks](#{typ}-attacks)\n"""
-        for j, atc in enumerate([k for k in sorted(attacks) if attacks[k]["types"][0] == typ]):
-            md_str += f"""       {j + 1}. [{attacks[atc]["name"]}](#{atc.replace("_", "-")})\n"""
-    md_str += """3. [Types](#types)
+        # Table of contents
+        for i, typ in enumerate(sorted(types)):
+            out += f"""   {i + 1}. [{typ.capitalize()} Poketes](#{typ}-poketes)\n"""
+            for j, poke in enumerate([k for k in sorted(list(pokes)[1:]) if pokes[k]["types"][0] == typ]):
+                out += f"""       {j + 1}. [{pokes[poke]["name"]}](#{poke.replace("_", "-")})\n"""
+        out += "2. [Attacks](#attacks)\n"
+        for i, typ in enumerate(sorted(types)):
+            out += f"""   {i + 1}. [{typ.capitalize()} attacks](#{typ}-attacks)\n"""
+            for j, atc in enumerate([k for k in sorted(attacks) if attacks[k]["types"][0] == typ]):
+                out += f"""       {j + 1}. [{attacks[atc]["name"]}](#{atc.replace("_", "-")})\n"""
+        out += """3. [Types](#types)
 4. [Items](#items)
 """
-    for j, item in enumerate(sorted(items)):
-        md_str += f"""   {j + 1}. [{items[item]["pretty_name"]}](#{item.replace("_", "-")})\n"""
-    md_str += """5. [Effects](#effects)
+        for j, item in enumerate(sorted(items)):
+            out += f"""   {j + 1}. [{items[item]["pretty_name"]}](#{item.replace("_", "-")})\n"""
+        out += """5. [Effects](#effects)
 """
-    for j, effect in enumerate(effect_list):
-        md_str += f"""   {j + 1}. [{effect.c_name.capitalize()}](#{effect.c_name.replace("_", "-")})
+        for j, effect in enumerate(effect_list):
+            out += f"""   {j + 1}. [{effect.c_name.capitalize()}](#{effect.c_name.replace("_", "-")})
 """
 
-    # Poketes
-    md_str += """
+        return out
+
+    @staticmethod
+    def poketes() -> str:
+        out = """
 ## Poketes
 In the following all Poketes with their attributes are displayed.
 
 """
-    for typ in sorted(types):
-        md_str += f"### {typ.capitalize()} Poketes"
-        for poke in [k for k in sorted(list(pokes)[1:]) if pokes[k]["types"][0] == typ]:
-            evolve_txt = f"""- Evolves to [{pokes[pokes[poke]["evolve_poke"]]["name"]}](#{pokes[poke]["evolve_poke"]}) at level {pokes[poke]["evolve_lvl"]}""" if \
-                pokes[poke]["evolve_poke"] != "" else "- Does not evolve\n"
-            md_attacks = ""
-            for atc in pokes[poke]["attacks"]:
-                md_attacks += f"""\n   + [{attacks[atc]["name"]}](#{atc.replace("_", "-")})"""
-            # ico
-            ico_map = se.Map(4, 11, background=" ")
-            for ico in pokes[poke]["ico"]:
-                se.Text(ico["txt"], state="float", ignore=" ").add(ico_map, 0, 0)
-            ico = "".join(["".join(arr) + "\n" for arr in ico_map.map])
-            md_str += f"""
+        for typ in sorted(types):
+            out += f"### {typ.capitalize()} Poketes"
+            for poke in [k for k in sorted(list(pokes)[1:]) if pokes[k]["types"][0] == typ]:
+                print(f' -> Adding {pokes[poke]["name"]}')
+                out += Wiki.poke_info(poke)
+        return out
+
+    @staticmethod
+    def poke_info(poke: str) -> str:
+        evolve_txt = f"""- Evolves to [{pokes[pokes[poke]["evolve_poke"]]["name"]}](#{pokes[poke]["evolve_poke"]}) at level {pokes[poke]["evolve_lvl"]}""" if \
+            pokes[poke]["evolve_poke"] != "" else "- Does not evolve\n"
+        md_attacks = ""
+        for atc in pokes[poke]["attacks"]:
+            md_attacks += f"""\n   + [{attacks[atc]["name"]}](#{atc.replace("_", "-")})"""
+        # ico
+        ico_map = se.Map(4, 11, background=" ")
+        for ico in pokes[poke]["ico"]:
+            se.Text(ico["txt"], state="float", ignore=" ").add(ico_map, 0, 0)
+        ico = "".join(["".join(arr) + "\n" for arr in ico_map.map])
+        return f"""
 #### {pokes[poke]["name"]}
 {pokes[poke]["desc"]}
 
@@ -78,29 +93,38 @@ In the following all Poketes with their attributes are displayed.
 {evolve_txt}
 """
 
-    # Attacks
-    md_str += """
+    @staticmethod
+    def attacks() -> str:
+        out = """
 ## Attacks
 Those are all attacks present in the game.
 """
-    for typ in sorted(types):
-        md_str += f"\n### {typ.capitalize()} attacks"
-        for atc in [k for k in attacks if attacks[k]["types"][0] == typ]:
-            eff = None if attacks[atc]["effect"] is None else getattr(effects, attacks[atc]["effect"])
-            md_str += f"""
-#### {attacks[atc]["name"]}
-{attacks[atc]["desc"]}
+        for typ in sorted(types):
+            out += f"\n### {typ.capitalize()} attacks"
+            for atc in [k for k in attacks if attacks[k]["types"][0] == typ]:
+                print(f' -> Adding {attacks[atc]["name"]}')
+                out += Wiki.attack_info(atc)
 
-- Type: [{attacks[atc]["types"][0].capitalize()}](#types)
-- Minimum Level: {attacks[atc]["min_lvl"]}
-- Attack factor: {attacks[atc]["factor"]}
-- Missing chance: {attacks[atc]["miss_chance"]}
-- Attack points: {attacks[atc]["ap"]}
+        return out
+
+    @staticmethod
+    def attack_info(attack: str) -> str:
+        eff = None if attacks[attack]["effect"] is None else getattr(effects, attacks[attack]["effect"])
+        return f"""
+#### {attacks[attack]["name"]}
+{attacks[attack]["desc"]}
+
+- Type: [{attacks[attack]["types"][0].capitalize()}](#types)
+- Minimum Level: {attacks[attack]["min_lvl"]}
+- Attack factor: {attacks[attack]["factor"]}
+- Missing chance: {attacks[attack]["miss_chance"]}
+- Attack points: {attacks[attack]["ap"]}
 - Effect: {"None" if eff is None else f'[{eff.c_name.capitalize()}](#{eff.c_name.replace("_", "-")})'}
 """
 
-    # Types
-    md_str += """
+    @staticmethod
+    def types() -> str:
+        out = """
 ## Types
 Those are all the Pokete/Attack types that are present in the game with all their (in)effectivities against other types.
 
@@ -108,22 +132,25 @@ Those are all the Pokete/Attack types that are present in the game with all thei
 |---|---|---|
 """
 
-    for poke_type in types:
-        effective, ineffective = ("".join([i.capitalize() + (", "
-                                                             if i != types[poke_type][j][-1]
-                                                             else "")
-                                           for i in types[poke_type][j]])
-                                  for j in ["effective", "ineffective"])
-        md_str += f"|{poke_type.capitalize()}|{effective}|{ineffective}|\n"
+        for poke_type in types:
+            effective, ineffective = ("".join([i.capitalize() + (", "
+                                                                 if i != types[poke_type][j][-1]
+                                                                 else "")
+                                               for i in types[poke_type][j]])
+                                      for j in ["effective", "ineffective"])
+            out += f"|{poke_type.capitalize()}|{effective}|{ineffective}|\n"
 
-    # Items
-    md_str += """
+        return out
+
+    @staticmethod
+    def items() -> str:
+        out = """
 ## Items
 Those are all items present in the game, that can be traded or found.
 """
 
-    for item in sorted(items):
-        md_str += f"""
+        for item in sorted(items):
+            out += f"""
 ### {items[item]["pretty_name"]}
 {items[item]["desc"]}
 
@@ -131,17 +158,39 @@ Those are all items present in the game, that can be traded or found.
 - Can be used in fights: {"Yes" if items[item]["fn"] is not None else "No"}
 """
 
-    # effects
-    md_str += """
+        return out
+
+    @staticmethod
+    def effects() -> str:
+        out = """
 ## Effects
 Those effects can be given to a Pokete through an attack.
 """
+        out += str.join("", [effect.ret_md() for effect in effect_list])
+        return out
 
-    md_str += str.join("", [effect.ret_md() for effect in effect_list])
+    @staticmethod
+    def single():
+        print(":: Generating wiki.md...")
+        print("==> Adding page start...")
+        md_str = Wiki.start()
+        print("==> Adding table of contents...")
+        md_str += Wiki.table_of_contents()
+        print("==> Adding poketes...")
+        md_str += Wiki.poketes()
+        print("==> Adding attacks...")
+        md_str += Wiki.attacks()
+        print("==> Adding types...")
+        md_str += Wiki.types()
+        print("==> Adding items...")
+        md_str += Wiki.items()
+        print("==> Adding effects...")
+        md_str += Wiki.effects()
 
-    # writing to file
-    with open("wiki.md", "w+") as file:
-        file.write(md_str)
+        # writing to file
+        print("==> Writing to wiki.md...")
+        with open("wiki.md", "w+") as file:
+            file.write(md_str)
 
 
 def gen_pics():
@@ -154,7 +203,6 @@ def gen_pics():
 
 
 if __name__ == "__main__":
-    print(":: Generating wiki.md...")
-    gen_wiki()
+    Wiki.single()
     print(":: Generating pics.md...")
     gen_pics()
