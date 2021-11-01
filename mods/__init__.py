14a5100abce8b0b98cb9c3bbf02986e7dabcde99
lxgr-linux <lxgr@protonmail.com>
2021-11-01 10:10:05 +0100

    improved ModError

--- mods/__init__.py
+++ mods/__init__.py
@@ -7,9 +7,10 @@ class ModError(Exception):
     An Error that is thrown, when an inproper module is loaded.
     """
 
-    def __init__(self, name):
+    def __init__(self, name, err):
         self.name = name
-        super().__init__(f"The mod '{name}' lacks attributes!")
+        self.err = err
+        super().__init__(f"The mod '{name}' lacks attributes!\n{err}")
 
 
 mod_names = [i.strip(".py") for i in os.listdir(__file__.strip("__init__.py"))
@@ -20,7 +21,7 @@ mod_obs = [importlib.import_module("mods."+i) for i in mod_names]
 for name, mod in zip(mod_names, mod_obs):
     try:
        [mod.version, mod.name, mod.mod_p_data]
-    except AttributeError:
-        raise ModError(name)
+    except AttributeError as err:
+        raise ModError(name, err)
 
 mod_info = {i.name : i.version for i in mod_obs}
