97179ae647ac089bcf0a2705711d9b1d583b7185
lxgr-linux <lxgr@protonmail.com>
2021-12-23 11:27:42 +0100

    The player now looses money on the way back to the center

--- pokete.py
+++ pokete.py
@@ -1213,9 +1213,12 @@ def check_walk_back(self=None):
     """Check whether the figure has to be walked back to the last Poketecenter
        or not"""
     if all(i.hp <= 0 for i in figure.pokes[:6]):
+        amount = round(figure.get_money() / 3)
+        figure.add_money(-amount)
         heal()
-        ask_ok(ev, movemap, """All your Poketes have died, and you ran
-back to the last Pokecenter to heal them!""")
+        ask_ok(ev, movemap, f"""All your Poketes have died and you ran
+back to the last Pokecenter you visited, to heal them!
+On the way there {amount}$ fell out of your pocket!""")
         figure.remove()
         figure.map = figure.last_center_map
         DorToCenter().action(figure)
