fd9607eb17a6aa5ccd2878bdb57ea00351f01bd7
lxgr-linux <lxgr@protonmail.com>
2022-01-04 00:07:24 +0100

    Added validation for achievements

--- pokete_data/__init__.py
+++ pokete_data/__init__.py
@@ -50,7 +50,7 @@ def single_validate(dict, validator, name=""):
 
 def validate():
     """Validates all modules"""
-    for i, j in zip([pokes, types, map_data, stations, items, npcs,
+    for i, j in zip([achievements, pokes, types, map_data, stations, items, npcs,
                      attacks, maps], validators):
         single_validate(i, j)
     for p in pokes:
@@ -68,6 +68,7 @@ def validate():
 
 
 validators = {
+    "achievements": ["title", "desc"],
     "poke": ["name", "hp", "atc", "defense", "attacks", "miss_chance", "desc",
              "lose_xp", "rarity", "types", "evolve_poke", "evolve_lvl", "ico",
              "initiative"],
