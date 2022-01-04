512d21d10eaae84b6bdfeb7064dc86812ecd70e8
lxgr-linux <lxgr@protonmail.com>
2022-01-04 01:43:17 +0100

    removed dummys

--- pokete_data/achievements.py
+++ pokete_data/achievements.py
@@ -5,36 +5,4 @@ achievements = {
         "title": "First Pokete",
         "desc": "Catch your first Pokete!"
     },
-    "dummy_1": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
-    "dummy_2": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
-    "dummy_3": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
-    "dummy_4": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
-    "dummy_5": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
-    "dummy_6": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
-    "dummy_7": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
-    "dummy_8": {
-        "title": "Dummy 1",
-        "desc": "Catch your first Pokete!"
-    },
 }
