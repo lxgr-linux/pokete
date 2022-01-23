6eb7a04f81719ce4fbb113556856fae251ac9cdc
lxgr-linux <lxgr@protonmail.com>
2022-01-23 14:47:11 +0100

    added thunderstorm

--- pokete_data/weather.py
+++ pokete_data/weather.py
@@ -7,6 +7,15 @@ weathers = {
             "water": 1.5
         }
     },
+    "thunderstorm": {
+        "info": "There is a thunderstorm going on!",
+        "effected": {
+            "fire": 0.5,
+            "plant": 1.5,
+            "water": 1.5,
+            "electro": 2,
+        }
+    },
     "foggy": {
         "info": "It's foggy!",
         "effected": {
