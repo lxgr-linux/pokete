4edd7577b06582b5ffbc19b4faf4fc0beabe07ba
lxgr-linux <lxgr@protonmail.com>
2022-04-21 00:43:08 +0200

    added docstrings

--- pokete_classes/nature.py
+++ pokete_classes/nature.py
@@ -1,3 +1,6 @@
+"""Contains everything related to the nature of Poketes, which influences some
+of their stats"""
+
 import random
 import logging
 from pokete_general_use_fns import liner
@@ -5,13 +8,27 @@ from .ui_elements import InfoBox
 from .loops import easy_exit_loop
 
 class Nature:
+    """The Nature class itself holds basic information about the Nature,
+    that means:
+    ARGS:
+        name: The nature name
+        atc: The attack change
+        _def: The defense change
+        init: The initiative change"""
+
     def __init__(self, name, atc=1, _def=1, init=1):
         self.name = name
         self.atc = atc
         self.defense = _def
         self.initiative = init
 
+
 class PokeNature:
+    """Holds specific information about a Pokete
+    ARGS:
+        nature: The Nature of the Pokete
+        grade: The Natures grade, 1 or 2"""
+
     natures = {name: Nature(name, **_dict) for name, _dict in {
         "brave": {
             "atc": 1.1,
@@ -33,25 +50,38 @@ class PokeNature:
         self.info = NatureInfo(self)
 
     def get_value(self, name):
+        """Gets one attribute value by
+        ARGS:
+            name"""
         return getattr(self.nature, name)
 
     def dict(self):
+        """RETURNS:
+            A dict containing inforamtion to reconstruct the object"""
         return {"nature": self.nature.name, "grade": self.grade}
 
     @classmethod
     def from_dict(cls, _dict):
+        """Constructs an object from a
+        ARGS:
+            _dict: dict"""
         nature = cls.natures.get(_dict.get("nature"), cls.natures["relaxed"])
         grade = _dict.get("grade", 1)
         return cls(nature, grade)
 
     @classmethod
     def random(cls):
+        """Creates an instance with random values"""
         nature = random.choice([i for _, i in cls.natures.items()])
         grade = random.randint(1, 2)
         return cls(nature, grade)
 
 
 class NatureInfo(InfoBox):
+    """Box to show information in Detail
+    ARGS:
+        p_n: PoketeNature object"""
+
     def __init__(self, p_n):
         atc = self.get_amount(p_n.nature.atc)
         defense = self.get_amount(p_n.nature.defense)
@@ -62,6 +92,7 @@ that  means it has {atc} attack, {defense} defense and {init} initiative then no
 
     @staticmethod
     def get_amount(val):
+        """Gets the amount denominator for a value"""
         if val == 1:
             return "the same"
         elif val < 1:
@@ -69,6 +100,9 @@ that  means it has {atc} attack, {defense} defense and {init} initiative then no
         return "more"
 
     def __call__(self, _map):
+        """Shows the box
+        ARGS:
+            _map: Map to show on"""
         self.map = _map
         with self:
             easy_exit_loop()
