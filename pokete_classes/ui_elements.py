c41f1a62cd9fcdf5286c069e6903e655c0bac149
BuildTools <unconfigured@null.spigotmc.org>
2022-06-18 11:08:20 +0200

    fixed bug that hid box name on resize

--- pokete_classes/ui_elements.py
+++ pokete_classes/ui_elements.py
@@ -72,6 +72,7 @@ class Box(se.Box):
         super().resize(height, width)
         self.inner.resize(width - 2, height - 2)
         self.frame.resize(height, width)
+        self.set_ob(self.name_label, 2, 0)
         self.set_ob(self.info_label, 2, self.height - 1)
 
     def add(self, _map, x, y):
