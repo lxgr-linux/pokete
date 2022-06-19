98bd6ecb4c36047f20399e56c2807b9832eee751
lxgr-linux <lxgr@protonmail.com>
2022-06-19 22:54:15 +0200

    added interventionfree migration

--- release.py
+++ release.py
@@ -5,5 +5,8 @@ from pathlib import Path
 
 VERSION = "0.7.2"
 CODENAME = "Grey Edition"
-SAVEPATH = os.environ.get("XDG_DATA_HOME", str(Path.home)+"/.local/share") + "/pokete"
+SAVEPATH = os.environ.get(
+    "XDG_DATA_HOME",
+    str(Path.home())+"/.local/share"
+) + "/pokete"
 FRAMETIME = 0.05
