9a2a6689f1bf7fe32051601a7cdf75d245de80e5
lxgr-linux <lxgr@protonmail.com>
2022-06-20 22:45:03 +0200

    added / seperators

--- release.py
+++ release.py
@@ -5,8 +5,10 @@ from pathlib import Path
 
 VERSION = "0.7.2"
 CODENAME = "Grey Edition"
-SAVEPATH = os.environ.get(
-    "XDG_DATA_HOME",
-    str(Path.home())+"/.local/share"
-) + "/pokete"
+SAVEPATH = Path(
+    os.environ.get(
+        "XDG_DATA_HOME",
+        str(Path.home())+"/.local/share"
+    )
+) / "pokete"
 FRAMETIME = 0.05
