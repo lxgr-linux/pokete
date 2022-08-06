2454b13e95317e289a46c70eac2748bb3fd62b22
lxgr-linux <lxgr@protonmail.com>
2022-08-06 04:17:23 +0200

    added MapChangeException to reduce giant stack traces

--- pokete_classes/game.py
+++ pokete_classes/game.py
@@ -1,6 +1,13 @@
-"""This is here to be able to globalize the game function"""
+"""Contains exception to exit a game loop and enter a new"""
 
-game = None
+class MapChangeExeption(Exception):
+    """Exception to exit a game loop and enter a new
+    ARGS:
+        _map: The new map to enter"""
+
+    def __init__(self, _map):
+        self.map = _map
+        super().__init__(f"Map change to {_map.name}")
 
 
 if __name__ == "__main__":
