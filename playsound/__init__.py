55471d12e1d86096b5f96ca65c6f99dd0f6804ec
ondrejmyska <ondrejmyska@users.noreply.github.com>
2022-08-22 20:38:36 +0200

    Update playsound/__init__.py
    Co-authored-by: lxgr-linux <64218491+lxgr-linux@users.noreply.github.com>

--- playsound/__init__.py
+++ playsound/__init__.py
@@ -7,7 +7,8 @@ from pathlib import Path
 def playsound(file, volume):
     """Plays a mp3 file
     ARGS:
-        file: path to file"""
+        file: path to file
+        volume: the sound volume as an int 0-100"""
     logging.info("[Playsound] playing %s", file)
     _playsound.playsound(file.encode("utf-8"), volume)
 
