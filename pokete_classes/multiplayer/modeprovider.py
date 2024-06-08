87a4fce68092ef5c48e7a938ea3555dcc9cb463b
lxgr-linux <lxgr@protonmail.com>
2024-06-08 13:34:44 +0200

    fix(#267): linting

--- pokete_classes/multiplayer/modeprovider.py
+++ pokete_classes/multiplayer/modeprovider.py
@@ -7,13 +7,13 @@ class Mode(Enum):
     """The modes the game can have"""
     SINGLE = auto()
     MULTI = auto()
-    
+
 
 class ModeProvider:
     """Holds the games mode"""
-    
+
     def __init__(self):
         self.mode: Mode = Mode.SINGLE
-        
+
 
 modeProvider = ModeProvider()
