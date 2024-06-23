426944762b3693d59f72cd67fea3129de17cf3c1
lxgr-linux <lxgr@protonmail.com>
2024-06-23 14:33:44 +0200

    feat(#287): Fixed icon

--- util/install.sh
+++ util/install.sh
@@ -1,7 +1,7 @@
 dest=$1
 
 install -Dv assets/com.github.lxgr-linux.pokete.desktop "$dest/share/applications/com.github.lxgr-linux.pokete.desktop"
-install -Dv assets/com.github.lxgr-linux.pokete.svg "$dest/share/icons/hicolor/scalable/apps/com.github.lxgr_linux.pokete.svg"
+install -Dvm644 assets/com.github.lxgr-linux.pokete.svg "$dest/share/icons/hicolor/scalable/apps/com.github.lxgr-linux.pokete.svg"
 install -Dv assets/pokete.metainfo.xml "$dest/share/metainfo/com.github.lxgr_linux.pokete.metainfo.xml"
 install -Dv LICENSE "$dest/share/licenses/com.github.lxgr_linux.pokete/LICENSE"
 install -dv "$dest/bin/"
