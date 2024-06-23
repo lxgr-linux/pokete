9828228cf6cb0681635ba4a487e8f24c6401cf6f
lxgr-linux <lxgr@protonmail.com>
2024-06-23 14:08:16 +0200

    feat(#287): Added install script

---
+++ util/install.sh
@@ -0,0 +1,12 @@
+dest=$1
+
+install -Dv assets/com.github.lxgr-linux.pokete.desktop "$dest/share/applications/com.github.lxgr-linux.pokete.desktop"
+install -Dv assets/com.github.lxgr-linux.pokete.svg "$dest/share/icons/hicolor/scalable/apps/com.github.lxgr_linux.pokete.svg"
+install -Dv assets/pokete.metainfo.xml "$dest/share/metainfo/com.github.lxgr_linux.pokete.metainfo.xml"
+install -Dv LICENSE "$dest/share/licenses/com.github.lxgr_linux.pokete/LICENSE"
+install -dv "$dest/bin/"
+find . | grep -E "\.py$|\.so$|\.mp3$" | while read file
+do
+    install -Dv "$file" "$dest/share/pokete/$file"
+done
+ln -s "../share/pokete/pokete.py" "$dest/bin/"
