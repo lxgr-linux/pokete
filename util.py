5465c76b2466fe9722f4fd67a3cd4a25665829ee
lxgr-linux <lxgr@protonmail.com>
2024-07-11 19:02:48 +0200

    feat(#287): Added appimage builder yaml to release flow

--- util.py
+++ util.py
@@ -30,7 +30,8 @@ def main():
                             prepare_after)
                 ]
             ),
-            Command("release", "Creates a release", make_release),
+            Command("release", "Prepare all relevant files for release",
+                    make_release),
             Command("wiki", "Generate a markdown wiki", gen_wiki, flags=[
                 wiki.silent_flag, wiki.quiet_flag, wiki.verbose_flag,
                 wiki.single_flag, wiki.multi_flag, wiki.pics_flag
