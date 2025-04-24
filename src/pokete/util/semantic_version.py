918b0f96ee33116dc2326adf9902263e54a5a66f
lxgr-linux <lxgr@protonmail.com>
2025-04-24 21:39:35 +0200

    feat(#308): Added greater operator

--- src/pokete/util/semantic_version.py
+++ src/pokete/util/semantic_version.py
@@ -1,7 +1,11 @@
+from functools import total_ordering
 from typing import Optional
 
 
+@total_ordering
 class SemanticVersion:
+    __suffix_order = ("rc", "beta", "alpha")
+
     def __init__(
         self, major: int, minor: int, patch: int,
         suffix: Optional[str] = None
@@ -25,3 +29,29 @@ class SemanticVersion:
         return f"{self.major}.{self.minor}.{self.patch}{
             ("-"+self.suffix) if self.suffix is not None else ""
         }"
+
+    def __eq__(self, value) -> bool:
+        other: "SemanticVersion" = value
+        return other.major == self.major and \
+            other.minor == self.minor and \
+            other.patch == self.patch and \
+            other.suffix == self.suffix
+
+    def __gt__(self, other: "SemanticVersion") -> bool:
+        for s, o in zip((self.major, self.minor, self.patch), (other.major, other.minor, other.patch)):
+            if s > o:
+                return True
+            elif s < o:
+                return False
+        if self.suffix == other.suffix:
+            return False
+        elif self.suffix is None:
+            return True
+        elif other.suffix is None:
+            return False
+        for suffix in self.__suffix_order:
+            if self.suffix.startswith(suffix):
+                if other.suffix.startswith(suffix):
+                    return int(self.suffix.strip(suffix)) > int(other.suffix.strip(suffix))
+                return True
+        return self.suffix > other.suffix
