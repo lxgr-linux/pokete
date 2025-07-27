b102147a01b5217f960f2032294239391da62eb1
lxgr-linux <lxgr@protonmail.com>
2025-07-27 18:29:41 +0200

    fix: fir for inv

--- src/pokete/classes/multiplayer/msg/fight/request.py
+++ src/pokete/classes/multiplayer/msg/fight/request.py
@@ -3,8 +3,8 @@ from typing import TypedDict
 import pokete.bs_rpc as bs_rpc
 from pokete.base.single_event import single_event_periodic_event
 from pokete.classes.multiplayer.remote_fight import MainThreatFightEvent
+import pokete.classes.multiplayer.pc_manager as pc_manager
 
-from ...pc_manager import pc_manager
 from .reponse import Response
 
 REQUEST_TYPE = "pokete.fight.request"
@@ -20,7 +20,7 @@ class Request(bs_rpc.Body):
 
     def call_for_response(self, context):
         name = self.data["name"]
-        rmtpl = pc_manager.get(name)
+        rmtpl = pc_manager.pc_manager.get(name)
         event = MainThreatFightEvent(name)
         single_event_periodic_event.add(event)
         accept = event.wait_accepted()
