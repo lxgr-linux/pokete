c162aaa23ca82cf43c2a86fa6e05ad1ba46ad61b
rogueLogical <watersliderchris@gmail.com>
2025-08-08 20:47:18 -0400

    Mock terminal size for running tests in github actions

--- src/tests/pokete/classes/poke/poke_test.py
+++ src/tests/pokete/classes/poke/poke_test.py
@@ -1,6 +1,9 @@
 import unittest
+from unittest.mock import patch
 
-from pokete.classes.poke import Poke
+with patch('os.get_terminal_size') as mock_get_terminal_size:
+    mock_get_terminal_size.return_value = (140, 40)
+    from pokete.classes.poke import Poke
 
 class AddXpTest(unittest.TestCase):
     def create_temp_pokete(self, current_xp):
