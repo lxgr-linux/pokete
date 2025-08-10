f6a6c434dea4d7df8bb10a7b291a4676052b5f31
Madison Maynard <madisontmaynard@gmail.com>
2025-08-10 13:17:58 -0400

    add bank_test

--- src/tests/pokete/classes/poke/bank_test.py
+++ src/tests/pokete/classes/poke/bank_test.py
@@ -1,10 +1,13 @@
 import unittest
-from unittest.mock import patch, mock_open
-from pokete.figure.bank import Bank
+from unittest.mock import patch
+
+with patch('os.get_terminal_size') as mock_get_terminal_size:
+    mock_get_terminal_size.return_value = (140, 40)
+    from pokete.figure.bank import Bank
 
 class bank_test(unittest.TestCase):
  
-
+    # negative test case
     def test_addMoneyNegative(self):
         try:
             Bank.__init__(self, -10)
@@ -18,6 +21,7 @@ class bank_test(unittest.TestCase):
         except ValueError as err:
             self.assertEqual(str(err), "Money cannot be negative")
  
+    # positive test case
     def test_addMoneyPositive(self):
         Bank.__init__(self, 10)
         self.assertEqual(Bank.get_money(self), 10)
