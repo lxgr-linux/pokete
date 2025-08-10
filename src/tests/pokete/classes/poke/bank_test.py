f5dcf1dfa1274851aebb8c7135f08f1c808ce76e
Madison Maynard <madisontmaynard@gmail.com>
2025-08-10 14:04:00 -0400

    updated and added more bank tests

--- src/tests/pokete/classes/poke/bank_test.py
+++ src/tests/pokete/classes/poke/bank_test.py
@@ -1,31 +1,43 @@
 import unittest
 from unittest.mock import patch
-
-with patch('os.get_terminal_size') as mock_get_terminal_size:
-    mock_get_terminal_size.return_value = (140, 40)
-    from pokete.figure.bank import Bank
+from pokete.figure.bank import Bank
+# with patch('os.get_terminal_size') as mock_get_terminal_size:
+#     mock_get_terminal_size.return_value = (140, 40)
+#     from pokete.figure.bank import Bank
 
 class bank_test(unittest.TestCase):
  
+    # negative init test case
+    def test_initMoneyNegative(self):
+        with self.assertRaises(AssertionError):
+            Bank(-10)
+
+    # positive init test case
+    def test_initMoneyPositive(self):
+        bank = Bank (10)
+        self.assertEqual(bank.get_money(), 10)
+
     # negative test case
     def test_addMoneyNegative(self):
+        bank = Bank (0)
         try:
-            Bank.__init__(self, -10)
-        except ValueError as err:
-            self.assertEqual(str(err), "Money cannot be negative")
+            bank.add_money(-10)
+        except AssertionError as err:
+            self.assertEqual(str(err), "Money has to be positive.")
 
-    # boundary test case
+    # boundary test case - add 0
     def test_addMoneyZero(self):
+        bank = Bank (0)
         try:
-            Bank.__init__(self, 0)
-        except ValueError as err:
-            self.assertEqual(str(err), "Money cannot be negative")
+            bank.add_money(0)
+        except AssertionError as err:
+            self.assertEqual(str(err), "Money has to be positive.")
  
     # positive test case
     def test_addMoneyPositive(self):
-        Bank.__init__(self, 10)
-        self.assertEqual(Bank.get_money(self), 10)
-
+        bank = Bank(0)
+        bank.add_money(10)
+        self.assertEqual(bank.get_money(), 10)
 
 if __name__ == "__main__":
     unittest.main()
