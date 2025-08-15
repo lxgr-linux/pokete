a9ac2d97008fb04bbaa350ae49539ca052e1a1c3
lxgr-linux <lxgr@protonmail.com>
2025-08-15 17:00:14 +0200

    feat(#327): Made menu mouse interactable

--- src/pokete/classes/menu.py
+++ src/pokete/classes/menu.py
@@ -4,13 +4,10 @@ from typing import Never, Optional
 
 import scrap_engine as se
 
-from pokete.base import loops
 from pokete.base.context import Context
-from pokete.base.input import Action, _ev, get_action
 from pokete.base.input.hotkeys import ActionList
 from pokete.base.input_loops import text_input
-from pokete.base.ui import Overview
-from pokete.base.ui.elements import ChooseBox, InfoBox
+from pokete.base.ui.elements import InfoBox
 from pokete.base.ui.notify import notifier
 from pokete.base.ui.views.choose_box import ChooseBoxView
 from pokete.release import SPEED_OF_TIME
@@ -26,18 +23,16 @@ from .side_loops import About
 class Menu(ChooseBoxView):
     def __init__(self):
         super().__init__(50, 35, "Menu")
-        self.playername_label = se.Text("Playername: ", state="float")
-        self.represent_char_label = se.Text("Char: ", state="float")
+        self.playername_input = TextInputBox("Playername:", 17)
+        self.represent_char_input = TextInputBox("Char:", 1)
         self.mods_label = se.Text("Mods", state="float")
         self.ach_label = se.Text("Achievements", state="float")
         self.about_label = se.Text("About", state="float")
         self.save_label = se.Text("Save", state="float")
         self.exit_label = se.Text("Exit", state="float")
-        self.realname_label = se.Text("", state="float")
-        self.char_label = se.Text("", state="float")
         self.elems = [
-            self.playername_label,
-            self.represent_char_label,
+            self.playername_input,
+            self.represent_char_input,
             VisSetting("Autosave", "autosave", {True: "On", False: "Off"}),
             VisSetting("Animations", "animations", {True: "On", False: "Off"}),
             VisSetting(
@@ -66,23 +61,11 @@ class Menu(ChooseBoxView):
     def choose(self, ctx: Context, idx: int) -> Optional[Never]:
         i = self.c_obs[self.index.index]
         audio_before = settings("audio").val
-        if i == self.playername_label:
-            ctx.figure.name = text_input(
-                ctx,
-                self.realname_label,
-                ctx.figure.name,
-                18,
-                17,
-            )
+        if i == self.playername_input:
+            ctx.figure.name = self.playername_input(ctx)
             self.map.name_label_rechar(ctx.figure.name)
-        elif i == self.represent_char_label:
-            inp = text_input(
-                ctx,
-                self.char_label,
-                ctx.figure.char,
-                18,
-                1,
-            )
+        elif i == self.represent_char_input:
+            inp = self.represent_char_input(ctx)
             # excludes bad unicode:
             if len(inp.encode("utf-8")) != 1 and inp not in [
                 "ä",
@@ -91,7 +74,7 @@ class Menu(ChooseBoxView):
                 "ß",
             ]:
                 inp = "a"
-                self.char_label.rechar(inp)
+                self.represent_char_input.set_value(inp)
                 notifier.notify(
                     "Error",
                     "Bad character",
@@ -126,156 +109,30 @@ valid single-space character!",
     def __call__(self, ctx: Context):
         """Opens the dex"""
         self.resize(ctx.map.height - 3, 35)
-        self.realname_label.rechar(ctx.figure.name)
-        self.char_label.rechar(ctx.figure.char)
+        self.playername_input.set_value(ctx.figure.name)
+        self.represent_char_input.set_value(ctx.figure.char)
         self.add_elems()
         with self.add(ctx.map, ctx.map.width - self.width, 0):
             return super().__call__(ctx)
 
 
-class Menu_(Overview):
-    """Menu to manage settings and other stuff in"""
+class TextInputBox(se.Box):
+    def __init__(self, label: str, max_len: int):
+        super().__init__(1, len(label) + 1 + max_len)
+        self.max_len = max_len
+        self.label = se.Text(label, state="float")
+        self.value = se.Text("", state="float")
+        self.add_ob(self.label, 0, 0)
+        self.add_ob(self.value, len(label) + 1, 0)
 
-    def __init__(self):
-        self.map = None
-        self.box = ChooseBox(50, 35, "Menu")
-        self.playername_label = se.Text("Playername: ", state="float")
-        self.represent_char_label = se.Text("Char: ", state="float")
-        self.mods_label = se.Text("Mods", state="float")
-        self.ach_label = se.Text("Achievements", state="float")
-        self.about_label = se.Text("About", state="float")
-        self.save_label = se.Text("Save", state="float")
-        self.exit_label = se.Text("Exit", state="float")
-        self.realname_label = se.Text("", state="float")
-        self.char_label = se.Text("", state="float")
-        self.box.add_c_obs(
-            [
-                self.playername_label,
-                self.represent_char_label,
-                VisSetting("Autosave", "autosave", {True: "On", False: "Off"}),
-                VisSetting(
-                    "Animations", "animations", {True: "On", False: "Off"}
-                ),
-                VisSetting(
-                    "Save trainers", "save_trainers", {True: "On", False: "Off"}
-                ),
-                VisSetting("Audio", "audio", {True: "On", False: "Off"}),
-                Slider("Volume", "volume"),
-                VisSetting(
-                    "Load mods", "load_mods", {True: "On", False: "Off"}
-                ),
-                self.mods_label,
-                self.ach_label,
-                self.about_label,
-                self.save_label,
-                self.exit_label,
-            ]
-        )
-        # adding
-        self.box.add_ob(
-            self.realname_label,
-            self.playername_label.rx + self.playername_label.width,
-            self.playername_label.ry,
-        )
-        self.box.add_ob(
-            self.char_label,
-            self.represent_char_label.rx + self.represent_char_label.width,
-            self.represent_char_label.ry,
+    def __call__(self, ctx: Context) -> str:
+        return text_input(
+            ctx,
+            self.value,
+            self.value.text,
+            self.max_len + 1,
+            self.max_len,
         )
 
-    def resize_view(self):
-        """Manages recursive view resizing"""
-        self.box.remove()
-        self.box.overview.resize_view()
-        self.box.resize(self.map.height - 3, 35)
-        self.box.add(self.map, self.map.width - self.box.width, 0)
-
-    def __call__(self, ctx: Context):
-        """Opens the menu"""
-        self.map = ctx.map
-        figure = ctx.figure
-        self.box.set_ctx(ctx)
-
-        self.box.resize(self.map.height - 3, 35)
-        self.realname_label.rechar(figure.name)
-        self.char_label.rechar(figure.char)
-        audio_before = settings("audio").val
-        volume_before = settings("volume").val
-        with self.box.add(self.map, self.map.width - self.box.width, 0):
-            _ev.clear()
-            while True:
-                action = get_action()
-                i = self.box.c_obs[self.box.index.index]
-                if (strength := action.get_x_strength()) != 0:
-                    if isinstance(i, Slider):
-                        i.change(strength)
-                elif action.triggers(Action.ACCEPT):
-                    # Fuck python for not having case statements - lxgr
-                    #     but it does lmao - Magnus
-                    if i == self.playername_label:
-                        figure.name = text_input(
-                            ctx.with_overview(self),
-                            self.realname_label,
-                            figure.name,
-                            18,
-                            17,
-                        )
-                        self.map.name_label_rechar(figure.name)
-                    elif i == self.represent_char_label:
-                        inp = text_input(
-                            ctx.with_overview(self),
-                            self.char_label,
-                            figure.char,
-                            18,
-                            1,
-                        )
-                        # excludes bad unicode:
-                        if len(inp.encode("utf-8")) != 1 and inp not in [
-                            "ä",
-                            "ö",
-                            "ü",
-                            "ß",
-                        ]:
-                            inp = "a"
-                            self.char_label.rechar(inp)
-                            notifier.notify(
-                                "Error",
-                                "Bad character",
-                                "The chosen character has to be a \
-valid single-space character!",
-                            )
-                        figure.rechar(inp)
-                    elif i == self.mods_label:
-                        ModInfo()(ctx.with_overview(self))
-                    elif i == self.save_label:
-                        # When will python3.10 come out?
-                        with InfoBox(
-                            "Saving....",
-                            info="",
-                            ctx=ctx.with_overview(self.box),
-                        ):
-                            # Shows a box displaying "Saving...." while saving
-                            save(figure)
-                            time.sleep(SPEED_OF_TIME * 1.5)
-                    elif i == self.exit_label:
-                        save(figure)
-                        sys.exit()
-                    elif i == self.about_label:
-                        About()(ctx.with_overview(self))
-                    elif i == self.ach_label:
-                        AchievementOverview()(ctx.with_overview(self))
-                    elif isinstance(i, VisSetting):
-                        i.change()
-                if (
-                    audio_before != settings("audio").val
-                    or volume_before != settings("volume").val
-                ):
-                    audio.play(figure.map.song)
-                    audio_before = settings("audio").val
-                    volume_before = settings("volume").val
-                elif action.triggers(Action.UP, Action.DOWN):
-                    self.box.input(action)
-                elif action.triggers(Action.CANCEL, Action.MENU):
-                    break
-                loops.std(ctx.with_overview(self))
-                self.map.full_show()
+    def set_value(self, value: str):
+        self.value.rechar(value)
