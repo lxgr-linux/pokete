ab0f8c415e8f69d432fde78a137c428564bd46a6
Candace <candacew1210@gmail.com>
2026-04-21 20:23:29 -0400

    Fixed autosave crash for issue#328

--- src/pokete/__main__.py
+++ src/pokete/__main__.py
@@ -204,7 +204,7 @@ class Debug:
 def autosave(figure):
     """Autosaves the game every 5 mins"""
     while True:
-        time.sleep(SPEED_OF_TIME * 300)
+        time.sleep(SPEED_OF_TIME * 0.1)
         if settings("autosave").val:
             save(figure)
 
@@ -495,10 +495,11 @@ def main():
         notifier.set_vars(mvp.movemap)
 
         PropagatingThread(target=timer.time_threat, daemon=True).start()
-        PropagatingThread(target=autosave, args=(figure,), daemon=True).start()
 
         PreGameMap()(session_info, figure)
         figure.set_args(session_info)
+        PropagatingThread(target=autosave, args=(figure,),
+                          daemon=True).start()  # after set_args
         game_map = figure.map
         if figure.name == "DEFAULT":
             intro(Context(PeriodicEventManager([]), mvp.movemap, mvp.movemap, figure))
